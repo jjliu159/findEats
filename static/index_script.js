3e2319416af32039dae89134dcfbfacd6b26a46a
Mandy Zhang <66440113+mandyyzhang@users.noreply.github.com>
2022-05-01 16:11:18 -0400

    fixed minor text

--- static/index_script.js
+++ static/index_script.js
@@ -9,13 +9,13 @@ let allIssues = [
   },
   {
     "name": "Free Reservation & Free Food",
-    "description": "Search, reserve, and pick up! That’s all there is to it. No fees or tax attached. Simply search for an address, see what is available that you like, place a reservation, pick up the goodie bag, and enjoy! Reservation is based on first come, first serve. ",
+    "description": "Search, reserve, and pick up! That’s all there is to it. No fees or tax attached. Simply search for an address, see what is available that you like, place a reservation, pick up the goodie bag, and enjoy! Reservation is based on first-come, first-serve. ",
     "picture": "food.png",
     // "article": "http://sitn.hms.harvard.edu/flash/2020/racial-discrimination-in-face-recognition-technology/"
   },
   {
     "name": "Help Reduce Food Waste",
-    "description": "Join us in fighting against the 40 million tons of food being thrown away annually. With food waste making up around 20% of landfill weight, FindEats is here to connect store owners to direct consumers in efforts to reduce wastfullness. ",
+    "description": "Join us in fighting against the 40 million tons of food being thrown away annually. With food waste making up around 20% of landfill weight, FindEats is here to connect store owners to direct consumers in efforts to reduce wastfulness. ",
     "picture": "rrr1.jpg",
     // "article": "https://www.technologyreview.com/2020/06/12/1003482/amazon-stopped-selling-police-face-recognition-fight/"
   }
